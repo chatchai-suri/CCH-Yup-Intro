cba94c212d6b74de0f671016efe1933f7c8ddc31
chatchai <chsuriyawan@yahoo.com>
2026-02-23 11:02:50 +0700

    input form add show/hide at password field

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,26 +7,31 @@ function Signup() {
     textError: "text-red-500 font-medium",
   };
 
-//------ State ------
+  //------ State ------
   const [formData, setFormData] = useState({
     username: "",
     nickname: "",
     password: "",
     confirmPassword: "",
     age: "",
-    terms: false
-  })
+    terms: false,
+  });
 
-  const [errors, setErrors] = useState({})
+  const [errors, setErrors] = useState({});
 
-//------ Function ------
-const hdlChange = (e) => {
-  const {name, value} = e.target
-  setFormData((prev)=>({...prev, [name]: value}))
-}
+  const [showPassword, setShowPassword] = useState(false);
 
+  //------ Function ------
+  const hdlChange = (e) => {
+    const { type, name, value, checked } = e.target;
+    if (type === "checkbox") {
+      setFormData((prev) => ({ ...prev, [name]: checked }));
+    } else {
+      setFormData((prev) => ({ ...prev, [name]: value }));
+    }
+  };
 
-//------ JSX area ------
+  //------ JSX area ------
   return (
     <div className="max-w-xl w-full shadow-lg bg-white rounded-lg p-2 flex flex-col justify-center items-center space-y-2">
       <h1 className="text-3xl text-accent-content bg-amber-200 w-full text-center rounded-md p-2">
@@ -39,27 +44,65 @@ const hdlChange = (e) => {
       >
         <div className={styles.inputDiv}>
           <label>Username:</label>
-          <input type="text" name="username" value={formData.username} onChange={hdlChange} className={styles.inputBox} />
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={hdlChange}
+            className={styles.inputBox}
+          />
         </div>
         <p className={styles.textError}>{`Error from Yup || &{errors}`}</p>
         <div className={styles.inputDiv}>
           <label>Nickname:</label>
-          <input type="text" name="nickname" value={formData.nickname} onChange={hdlChange} className={styles.inputBox} />
+          <input
+            type="text"
+            name="nickname"
+            value={formData.nickname}
+            onChange={hdlChange}
+            className={styles.inputBox}
+          />
         </div>
         <p className={styles.textError}>{`Error from Yup || &{errors}`}</p>
         <div className={styles.inputDiv}>
           <label>Password:</label>
-          <input type="password" name="password" value={formData.password} onChange={hdlChange} className={styles.inputBox} />
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            value={formData.password}
+            onChange={hdlChange}
+            className={styles.inputBox}
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="btn btn-md btn-ghost"
+          >
+            {showPassword ? "Hide" : "Show"}
+          </button>
         </div>
         <p className={styles.textError}>{`Error from Yup || &{errors}`}</p>
         <div className={styles.inputDiv}>
           <label>Age:</label>
-          <input type="password" name="confirmPassword" value={formData.confirmPassword} onChange={hdlChange} className={styles.inputBox} />
+          <input
+            type="text"
+            name="age"
+            value={formData.age}
+            onChange={hdlChange}
+            className={styles.inputBox}
+          />
         </div>
         <p className={styles.textError}>{`Error from Yup || &{errors}`}</p>
         <div className={styles.inputDiv}>
-          <input type="checkbox" name="username"  className={styles.inputBox} />
-          <label>Read and Accepted Terms</label>
+          <label className="hover:font-bold hover:cursor-pointer">
+            <input
+              type="checkbox"
+              name="terms"
+              value={formData.terms}
+              onChange={hdlChange}
+            />
+            Read and Accepted Terms
+          </label>
         </div>
         <p className={styles.textError}>{`Error from Yup || &{errors}`}</p>
         <div className="flex justify-center">
@@ -68,6 +111,9 @@ const hdlChange = (e) => {
           </button>
         </div>
       </form>
+      <div className="text-center">
+        <pre>{JSON.stringify(formData, null, 2)}</pre>
+      </div>
     </div>
   );
 }
